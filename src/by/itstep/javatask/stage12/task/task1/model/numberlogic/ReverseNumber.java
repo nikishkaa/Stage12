7114528b6fd12ac8abc95d024dd6b8c4cfd9df2e
nikishkaa <nikita.goutor@gmail.com>
2024-02-13 15:04:18 +0300

    Update task 1

--- src/by/itstep/javatask/stage12/task/task1/model/numberlogic/ReverseNumber.java
+++ src/by/itstep/javatask/stage12/task/task1/model/numberlogic/ReverseNumber.java
@@ -3,7 +3,7 @@ package by.itstep.javatask.stage12.task.task1.model.numberlogic;
 public class ReverseNumber {
     public static int getReverseNumber(int number) {
 
-        number = number > 0 ? number : -number; //12
+        number = number > 0 ? number : -number;
 
         int reverseNumber = 0;
 
